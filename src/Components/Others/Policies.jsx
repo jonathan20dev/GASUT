1acc38ee279d84df22d9baf965ddbb6901676a91
cris-gs <cristophergs2001@gmail.com>
2022-06-07 19:29:53 -0600

    acerca de y politicas

--- src/Components/Others/Policies.jsx
+++ src/Components/Others/Policies.jsx
@@ -12,13 +12,13 @@ export const Policies = () => {
             <h2 className='text-center m-5 fw-bold'>Políticas de GASUT</h2>
             <div className="m-5">
                 <h4 className='fw-bold'>Soporte</h4>
-                <p className='my-3 mx-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='my-3 mx-5 text-justify'>La aplicación busca estar en constante actualización para mejorar la funcionalidad de esta misma, de esta manera estar pendiente a las sugerencias de los usuarios. Por medio de la información de contacto puede mantener comunicación con nuestro equipo, este según el horario establecido. También cuenta con nuestras redes sociales para mantenerse más informado sobre las novedades y decisiones de nuestra empresa, para nosotros es un gusto atenderle.</p>
                 
                 <h4 className='fw-bold'>Datos Personales</h4>
-                <p className='my-3 mx-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='my-3 mx-5 text-justify'>GASUT tiene la intención de retener los datos personales que recopila a través del Servicio, siempre y cuando sigan siendo útiles para los fines de seguridad que se describen a continuación.  Se realizarán pruebas y análisis periódicos para determinar y confirmar este período de retención. Los datos guardados en las copias de seguridad pueden conservarse más tiempo que los datos originales.</p>
                 
                 <h4 className='fw-bold'>Propiedad y responsabilidad</h4>
-                <p className='my-3 mx-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='my-3 mx-5 text-justify'>Debe tener en cuenta que GASUT no se hace responsable de situaciones externas a la oferta de los productos y servicios. Queda bajo responsabilidad de los usuarios el fin con el que oferta por medio de la aplicación, sin embargo, como parte del trabajo de GASUT se debe de estar realizando un análisis riguroso de los productos o servicios publicados, velando porque estos tengan un carácter legal.</p>
             </div>
           </div>
         </div>
